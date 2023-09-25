8894d0efcbd80f03bf2b198317be9b6c8440a145
gauravgarwa <gauravgarwa033@gmail.com>
2023-09-26 02:27:48 +0530

    push001.1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
 import Chart from '../../chart/App'
-const layout = ({children} : {childeren : React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
     <Navbar/>
