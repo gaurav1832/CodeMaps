1701cc895777059f44f35215df6c37dd8857f0f0
gauravgarwa <gauravgarwa033@gmail.com>
2023-10-08 00:37:54 +0530

    push002.1.4

--- components/intro.tsx
+++ components/intro.tsx
@@ -80,10 +80,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
-          }}
+          // onClick={() => {
+          //   setActiveSection("Contact");
+          //   setTimeOfLastClick(Date.now());
+          // }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
