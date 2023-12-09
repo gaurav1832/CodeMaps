5a65c9ac7f1a3133042b642ebd45d76dcd15de97
gauravgarwa <gauravgarwa033@gmail.com>
2023-12-09 23:48:11 +0530

    push

--- app/(algorithms)/algorithms/layout.tsx
+++ app/(algorithms)/algorithms/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/Header"
-import Footer from "@/components/Footer";
+import Header from "../../../components/Header"
+import Footer from "../../../components/Footer";
 import "../../globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
